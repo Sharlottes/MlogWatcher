6e4e4b9c82ce1630c23b8b78738bb8833b681370
Sharlotte <aaa9810321@gmail.com>
2023-12-25 23:29:53 +0900

    style: import clean up

--- src/mlogwatcher/Setting.java
+++ src/mlogwatcher/Setting.java
@@ -1,15 +1,10 @@
 package mlogwatcher;
 
 import arc.Core;
-import arc.scene.Group;
-import arc.scene.ui.Dialog;
 import arc.scene.ui.Label;
 import arc.scene.ui.TextField;
-import arc.scene.ui.layout.*;
 
 import mindustry.Vars;
-import mindustry.ui.Styles;
-import mindustry.ui.dialogs.BaseDialog;
 
 public class Setting {
     public static void init(){
@@ -25,14 +20,14 @@ public class Setting {
             settingTable.center();
             settingTable.add(Constants.Bundles.settingMlogPathLabel).row();
             settingTable.add(label).row();
-            settingTable.button(Constants.Bundles.settingMlogSelectButton, () -> {
+            settingTable.button(Constants.Bundles.settingMlogSelectButton, () ->
                 Vars.platform.showFileChooser(true, Constants.Bundles.settingFileChooserTitle, field.getText(), fi -> {
                     label.setText(fi.absolutePath());
                     Core.settings.put(Constants.Settings.mlogPath, fi.absolutePath());
                     FileWatcher.stopWatcherThread();
                     FileWatcher.startWatcherThread();
-                });
-            }).width(280f).height(60f).pad(16f).row();
+                })
+            ).width(280f).height(60f).pad(16f).row();
             settingTable.table(fieldTable -> {
                 fieldTable.add(Constants.Bundles.settingExtensionInputLabel);
                 fieldTable.add(field);
